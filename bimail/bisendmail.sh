eeaf701c85090e8d46c93eba90c2449102f9c246
LiNan <believelinan@aliyun.com>
2020-09-01 18:31:26 +0800

    bimail: update bisendmail.sh

--- bimail/bisendmail.sh
+++ bimail/bisendmail.sh
@@ -5,9 +5,11 @@ cd "$workdir"
 
 date=$(date +%F)
 subject="BI Dashboard Database Daily Report ${date}"
+from='nan2.li@ge.com'
 tomail='zhe.hu@ge.com'
 ccmail='nan2.li@ge.com,blake.xue@ge.com'
 body='biquery.txt'
 
 # mail -s "${subject}" nan2.li@ge.com < "${body}"
-mail -s "${subject}" -c ${ccmail} ${tomail} < "${body}"
+# mail -s "${subject}" -r ${from} nan2.li@ge.com < "${body}"
+mail -s "${subject}" -r ${from} -c ${ccmail} ${tomail} < "${body}"
