5efc42a28196bba56b0b09cd5c6472bfe6491b3e
LiNan <believelinan@aliyun.com>
2020-09-01 19:11:03 +0800

    bimail: update biquery.sh by date -d "+8 hours" "+%F %T"

--- bimail/biquery.sh
+++ bimail/biquery.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 #
 
-echo **Query Time: $(date "+%F %T")** > biquery.txt
+echo **Query Time: $(date -d "+8 hours" "+%F %T")** > biquery.txt
 echo "**This is automatic mail, please do not reply!**" >> biquery.txt
 
 echo>> biquery.txt
@@ -10,4 +10,15 @@ mysql -hestar.xlink.cloud -uroot -pGEMysql20200 -e 'select * from GE.summary ord
 echo>> biquery.txt
 echo ==2. Active Table== >> biquery.txt
 mysql -hestar.xlink.cloud -uroot -pGEMysql20200 -e 'select * from GE.active order by statistics_time desc limit 5;' >> biquery.txt
+echo>> biquery.txt
+echo ==3. Device_produced Table== >> biquery.txt
+mysql -hestar.xlink.cloud -uroot -pGEMysql20200 -e 'select created_time,sum(produced_count) from GE.device_produced group by created_time order by created_time desc limit 5;' >> biquery.txt
+echo>> biquery.txt
+echo ==4. Device Table== >> biquery.txt
+mysql -hestar.xlink.cloud -uroot -pGEMysql20200 -e 'select count(appliance_id) as 'Total' from GE.device;' >> biquery.txt
+mysql -hestar.xlink.cloud -uroot -pGEMysql20200 -e 'select device_type, count(appliance_id) as 'count' from GE.device group by device_type order by count desc;' >> biquery.txt
+
+
+
+
 
